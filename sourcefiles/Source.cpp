ba15f6dbd0f298d6e9a66f2984b31a693c77b72d
shaun965 <shaun965@hotmail.com>
2014-11-08 01:31:47 +1300

    asd
    asdads

---
+++ sourcefiles/Source.cpp
@@ -0,0 +1,49 @@
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+		sf::Texture texture;
+		if (!texture.loadFromFile("image2.png"))
+		{
+		}
+
+		sf::Sprite sprite;
+        sprite.setTexture(texture);
+		//sprite.getColor();
+        //sprite.setColor(sf::Color(0, 255, 0));
+		sprite.setScale	(0.4, 0.3);
+		window.draw(sprite);
+		
+
+        // draw everything here...
+        // window.draw(...);
+
+        // end the current frame
+        window.display();
+    }
+
+    return 0;
+}
