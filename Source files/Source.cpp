401b8aec992309f6ecc3df36be6a349b9f896d83
milo168 <macronie44@gmail.com>
2014-11-08 22:06:37 -0800

    updated Source

--- Source files/Source.cpp
+++ Source files/Source.cpp
@@ -6,16 +6,25 @@ int main()
 	window.setFramerateLimit(60);
 
 	bool state = true;
+
 	sf::Font gameOver;
 	gameOver.loadFromFile("arial.ttf");
 	sf::Text Text;
+
 	Text.setFont(gameOver);
 	Text.setString("GAME OVER");
 	Text.setCharacterSize(75);
 	Text.setPosition(200, 250);
 	Text.setColor(sf::Color::Red);
 
-	Floor *floor = new Floor();
+	sf::Sprite *menu = new sf::Sprite();
+	sf::Texture *menuTexture = new sf::Texture();
+
+	menuTexture->loadFromFile("menu.png");
+	menu->setTexture(*menuTexture);
+	menu->setScale(1.12, 1.85);
+
+	Floor *floor = new Floor(2,1);
 	int n = 0;
 
 	while (window.isOpen())
@@ -39,13 +48,31 @@ int main()
 				floor->getPlayer()->UpdatePosition(0, 0);
 			}
 
+			if (event.type == sf::Event::MouseButtonPressed){
+				if (state == false && sf::Mouse::getPosition().y > 300 && n > 100){
+					window.close();
+				}
+				if (state == false && sf::Mouse::getPosition().y < 300 && n > 100){
+					state = true;
+					n = 0;
+					for (int i = 0; i < floor->getTotalGhosts(); i++){
+						floor->getMonster()[i].getSprite()->setPosition(100 + getRandomInt(200, 600), 400);
+						floor->getMonster()[i].getSprite()->setScale(.5, .5);
+						std::cout << i << std::endl;
+					}
+				}
+			}
 		}
 
-		floor->getMonster()->UpdatePosition();
+		for (int i = 0; i < floor->getTotalGhosts(); i++){
+			floor->getMonster()[i].UpdatePosition();
+		}
 
 		window.clear();
 
-		collision(floor->getPlayer(), floor->getMonster()->getSprite(), state);
+		for (int i = 0; i < floor->getTotalGhosts(); i++){
+			collision(floor->getPlayer(), floor->getMonster()[i].getSprite(), state);
+		}
 
 		for (auto it = floor->getObjects()->begin(); it != floor->getObjects()->end(); it++){
 			window.draw(**it);
@@ -53,19 +80,21 @@ int main()
 
 		if (state == false){
 			window.draw(Text);
+			n++;
+			if (n > 100){
+				window.draw(*menu);
+			}
 		}
 
 		window.display();
-
 		window.clear();
 
 		if (state == false && n % 1000 == 0){
-			floor->getMonster()->UpdatePosition();
-			floor->getMonster()->getSprite()->setPosition(-10, 0);	
+			floor->getMonster()[0].UpdatePosition();
+			floor->getMonster()[0].getSprite()->setPosition(-10, 0);	
 			window.display();
 		}
 
-		n++;
 	}
 
 	system("pause");
